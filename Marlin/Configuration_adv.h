16d97e2d9f2fce323f7433b79a04065ff9858f2d
jmtome <jtome@fi.uba.ar>
2025-06-29 18:59:42 -0300

    Thermal Runaway values that currently work... To be checked.

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -296,8 +296,8 @@
  * THERMAL_PROTECTION_HYSTERESIS and/or THERMAL_PROTECTION_PERIOD
  */
 #if ENABLED(THERMAL_PROTECTION_HOTENDS)
-  #define THERMAL_PROTECTION_PERIOD 40        // (seconds)
-  #define THERMAL_PROTECTION_HYSTERESIS 4     // (°C)
+  #define THERMAL_PROTECTION_PERIOD 200       // (seconds) - much longer for slow system
+  #define THERMAL_PROTECTION_HYSTERESIS 50     // (°C) - handle thermistor instability at 235°C
 
   //#define ADAPTIVE_FAN_SLOWING              // Slow part cooling fan if temperature drops
   #if BOTH(ADAPTIVE_FAN_SLOWING, PIDTEMP)
@@ -316,8 +316,8 @@
    * and/or decrease WATCH_TEMP_INCREASE. WATCH_TEMP_INCREASE should not be set
    * below 2.
    */
-  #define WATCH_TEMP_PERIOD  40               // (seconds)
-  #define WATCH_TEMP_INCREASE 2               // (°C)
+  #define WATCH_TEMP_PERIOD  120              // (seconds) - longer for slow heating
+  #define WATCH_TEMP_INCREASE 1               // (°C) - smaller increase required
 #endif
 
 /**
@@ -330,8 +330,8 @@
   /**
    * As described above, except for the bed (M140/M190/M303).
    */
-  #define WATCH_BED_TEMP_PERIOD                60 // (seconds)
-  #define WATCH_BED_TEMP_INCREASE               2 // (°C)
+  #define WATCH_BED_TEMP_PERIOD                80 // (seconds)
+  #define WATCH_BED_TEMP_INCREASE               1 // (°C)
 #endif
 
 /**
