4ed90814a4f63b0ec7a96eb81bf9ebf91ff43310
jmtome <jtome@fi.uba.ar>
2025-12-03 15:46:50 -0300

    Changed Driver type
    Changed Driver type for X and Y axis from A4988 tp TMC2130_STANDALONE, also inverted directions as it was necessary

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -161,8 +161,10 @@
  *          TMC5130, TMC5130_STANDALONE, TMC5160, TMC5160_STANDALONE
  * :['A4988', 'A5984', 'DRV8825', 'LV8729', 'TB6560', 'TB6600', 'TMC2100', 'TMC2130', 'TMC2130_STANDALONE', 'TMC2160', 'TMC2160_STANDALONE', 'TMC2208', 'TMC2208_STANDALONE', 'TMC2209', 'TMC2209_STANDALONE', 'TMC26X', 'TMC26X_STANDALONE', 'TMC2660', 'TMC2660_STANDALONE', 'TMC5130', 'TMC5130_STANDALONE', 'TMC5160', 'TMC5160_STANDALONE']
  */
-#define X_DRIVER_TYPE  A4988
-#define Y_DRIVER_TYPE  A4988
+// #define X_DRIVER_TYPE  A4988
+#define X_DRIVER_TYPE TMC2130_STANDALONE
+// #define Y_DRIVER_TYPE  A4988
+#define Y_DRIVER_TYPE TMC2130_STANDALONE
 #define Z_DRIVER_TYPE  A4988
 //#define X2_DRIVER_TYPE A4988
 //#define Y2_DRIVER_TYPE A4988
@@ -1670,8 +1672,8 @@
 // @section motion
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR true
-#define INVERT_Y_DIR false
+#define INVERT_X_DIR false
+#define INVERT_Y_DIR true
 #define INVERT_Z_DIR true
 //#define INVERT_I_DIR false
 //#define INVERT_J_DIR false
