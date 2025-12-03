9cad57e41b900d9af21aaf9ef80cc44b74339679
jmtome <jtome@fi.uba.ar>
2025-12-03 15:49:19 -0300

    Temperature Hystereses changes
    Changed timings and temperatures to avoid thermal runaway

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -324,13 +324,13 @@
  * Thermal Protection parameters for the bed are just as above for hotends.
  */
 #if ENABLED(THERMAL_PROTECTION_BED)
-  #define THERMAL_PROTECTION_BED_PERIOD        20 // (seconds)
-  #define THERMAL_PROTECTION_BED_HYSTERESIS     2 // (°C)
+  #define THERMAL_PROTECTION_BED_PERIOD        60 // (seconds)
+  #define THERMAL_PROTECTION_BED_HYSTERESIS     5 // (°C)
 
   /**
    * As described above, except for the bed (M140/M190/M303).
    */
-  #define WATCH_BED_TEMP_PERIOD                80 // (seconds)
+  #define WATCH_BED_TEMP_PERIOD                120 // (seconds)
   #define WATCH_BED_TEMP_INCREASE               1 // (°C)
 #endif
 
